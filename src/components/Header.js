0a3d8cf1f59e0fc0672f2a14eee7621017a1b699
priyanshukr449 <priyanshukr449@gmail.com>
2023-10-11 23:28:42 +0530

    fetching data from movie api

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,23 +2,45 @@ import React from "react";
 import {auth} from '../utils/firebase'
 import {signOut} from "firebase/auth"
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Header = () => {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+
     const user = useSelector(store => store.user)
 
     const signOutHandler = () =>{
         signOut(auth)
         .then(() => {
-            // Sign-out successful.
-            console.log("Sign Out Successful");
-            navigate('/')
         })
         .catch((error) => {
                 navigate('/error')
         });
     }
+
+    useEffect(() => {
+        // whenever user sign in or sign up this function will called
+        const unsubscribe = onAuthStateChanged(
+            auth,
+            (user) => {
+                if (user) {
+                    const {uid,displayName,email,photoURL} = user;
+                    dispatch(addUser({uid:uid,email:email,displayName:displayName,photoURL:photoURL}))
+                    navigate('/browse')
+                } else {
+                    // User is signed out
+                    dispatch(removeUser())
+                    navigate('/')
+                }
+            }
+        );
+        return () => unsubscribe();
+    },[]);
+
     return (
         <div className="absolute w-full px-16 py-1 z-10 bg-gradient-to-b from-black flex justify-between">
             <div>
---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const options = {
+    method: "GET",
+    headers: {
+        accept: "application/json",
+        Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjg2OWExNGQ5ZDdkNGE3Y2NhNWU0ZDYyOWYyMjBiNSIsInN1YiI6IjY1MjZjYzU1ZWE4NGM3MDBjYTBlYmI5MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CTPKCj13Q4asSmchQQKryA7mB97Tr0RIW6AQNq7ETPQ",
+    },
+};
