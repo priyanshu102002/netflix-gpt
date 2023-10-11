c405c350ba5dd0a0eed0f32798b9f090c56f8c5c
priyanshukr449 <priyanshukr449@gmail.com>
2023-10-11 23:30:57 +0530

    some changeg in fetching the data

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,6 @@ export const options = {
     headers: {
         accept: "application/json",
         Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjg2OWExNGQ5ZDdkNGE3Y2NhNWU0ZDYyOWYyMjBiNSIsInN1YiI6IjY1MjZjYzU1ZWE4NGM3MDBjYTBlYmI5MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CTPKCj13Q4asSmchQQKryA7mB97Tr0RIW6AQNq7ETPQ",
+            `Bearer ${import.meta.REACT_MOVIE_API_TOKEN}`,
     },
 };
